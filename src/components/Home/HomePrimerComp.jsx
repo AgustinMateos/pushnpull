2d4ba7583f867d0f47989ad61e998e92cc2646a2
agustin mateos <agustinrotondomateos@gmail.com>
2024-02-22 20:29:23 -0300

    index mobile

--- src/components/Home/HomePrimerComp.jsx
+++ src/components/Home/HomePrimerComp.jsx
@@ -12,15 +12,15 @@ const HomePrimerComp = () => {
           <h2 className='homePrimerCompTitulo'>Developer Resources</h2>
           <p className='homePrimerCompTexto font-family-Robot'>Push n Pull es una agencia digital basada en datos especializada en brindar soluciones digitales.</p>
         </div>
-        <div className="flex-justify-start homePrimerbuttonContainerMobile " >
+        <div className=" homePrimerCompButtons  " >
           <Link to={'/ServicesPage'} className='buttonVermas flex-align-center '><span className='buttonVermasContent font-family-Robot'>Nuestros servicios</span></Link>
-          <Link to={'/ContactoPage'} className='buttonVermas2 flex-align-center '><span className='buttonVermasContent2 font-family-Robot'>Contactanos</span></Link>
+          <Link to={'/ContactoPage'} className='buttonVermas2  '><span className='buttonVermasContent2 font-family-Robot'>Contactanos</span></Link>
         </div>
 
        
 
       </div>
-      <div className='wd-40 h-100 imagenlatidoContainer flex-align-center flex-justify-center '>
+      <div className='homePrimerCompContainerImg h-100 imagenlatidoContainer flex-align-center flex-justify-center  '>
         <img className='wd-100 h-90  homePrimerCompImg imagen-latido' src={grafica8} alt="" /> 
       </div>
     </div>
