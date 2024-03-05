db99ab4f7170854c50a9f818a64ba5ddd1ff6cbc
agustin mateos <agustinrotondomateos@gmail.com>
2024-03-05 01:46:22 -0300

    img servicios

--- src/components/Seo/SeoBody.jsx
+++ src/components/Seo/SeoBody.jsx
@@ -1,81 +1,99 @@
+
+
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { seoServiciosData } from '../../data/SeoServiciosData';
-import solucionesDigitalesBody from '../../assets/solucionesDigitalesPushnPull.webp';
+
+import seoImg from '../../assets/ecommerceGrafica.png';
+import seo from '../../assets/ecommerceGrafica.png'
 import whatsapp from "../../assets/whatsapp.png"
+import mantenimientoSeo from '../../assets/desarrolloweb.png'
+import solucionesDigitalesImg from '../../assets/solucionesDigitalesPushnPull.webp'
+import  desarrolloWeb from '../../assets/desarrolloweb.png'
 
 
-const SeoBody = () => {
-  const servicios = seoServiciosData;
-  const [servicioActivo, setServicioActivo] = useState(servicios[0]);
 
-  const handleChangeServicio = (servicio) => {
-    setServicioActivo(servicio);
-    console.log(servicio);
+const MantenimientoSeo = () => {
+  const [selectedOption, setSelectedOption] = useState('Opción 1'); // Inicializando con 'Opción 1'
+
+  const handleOptionClick = (option) => {
+    setSelectedOption(option);
   };
-
   return (
     <div className='wd-100'>
-      <a className='whatsapp'  href="https://wa.me/"><img src={whatsapp} alt="" /></a>
-    <div className='HomePrimerComp  '>
-    <div className='homePrimerCompContainer'>
-      <div>
-        <h2 className='homePrimerCompTitulo'>Seo</h2>
-        <p className='homePrimerCompTexto font-family-Robot'>Push n Pull es una agencia digital basada en datos especializada en brindar soluciones digitales.</p>
+      <a className='whatsapp' href="https://wa.me/"><img src={whatsapp} alt="" /></a>
+      <div className='HomePrimerComp  '>
+        <div className='homePrimerCompContainer'>
+          <div>
+            <h2 className='homePrimerCompTitulo'>Seo</h2>
+            <p className='homePrimerCompTexto font-family-Robot'>Push n Pull es una agencia digital basada en datos especializada en brindar soluciones digitales.</p>
+          </div>
+
+
+
+
+        </div>
+        <div className='homePrimerCompContainerImg h-100 imagenlatidoContainer flex-align-center flex-justify-center  '>
+          <img className='wd-100 h-90  homePrimerCompImg imagen-latido' src={solucionesDigitalesImg} alt="" />
+        </div>
       </div>
-      
-
-     
-
-    </div>
-    <div className='homePrimerCompContainerImg h-100 imagenlatidoContainer flex-align-center flex-justify-center  '>
-      <img className='wd-100 h-90  homePrimerCompImg imagen-latido' src={solucionesDigitalesBody} alt="" /> 
-    </div>
-  </div>
-    
-    <div className='wd-100 flex-justify-center'>
-      <div className='wd-90 flex-justify-center flex-column  mantenimientoContainerHeight'>
-        <div className='wd-100 flex-justify-center flex-justify-center'>
-
+      <div className='wd-100 flex-justify-center'>
+        <div className='wd-90 flex-justify-center flex-column  mantenimientoContainerHeight'>           <div className='wd-100 flex-justify-center flex-justify-center'>
           <div className='wd-100 flex-align-center flex-justify-center flex-column'>
 
-            <div className='flex-column wd-100 flex-justify-space-around mantenimientoContainerRow'>
-              {servicios.map((servicio, index) => (
-                <Link className='buttonVermas2 flex-align-center' key={index} onClick={() => {
-                  handleChangeServicio(servicio)
-                  console.log(servicio.imagen)
-                }}>
-                  {servicio.categoria}
-                </Link>
-              ))}
-            </div>
+            {/* <div className='flex-column wd-100 flex-justify-space-around mantenimientoContainerRow'>                  */}
+            <ul className='flex-column wd-100 flex-justify-space-around mantenimientoContainerRow'>
+              <li className='buttonVermas2 flex-align-center' onClick={() => handleOptionClick('Opción 1')}>Estrategia Seo</li>
+              <li className='buttonVermas2 flex-align-center' onClick={() => handleOptionClick('Opción 2')}>Mantenimiento Seo</li>
+              <li className='buttonVermas2 flex-align-center' onClick={() => handleOptionClick('Opción 3')}>Mantenimeinto de Base de Datos</li>
+            </ul>
+
+            {/* </div> */}
           </div>
         </div>
-        <div className='wd-100 flex-justify-center '>
-          <div className='wd-100 mantenimientoContainerInfo '>
-            <img className=' mantenimientoContainerInfoImg h-100' src={servicioActivo.img} alt='' />
-            <div className='mantenimientoContainerInfoTexto  '>
-              <h5 className='titulosComponentes'>{servicioActivo.titulo}</h5>
-              <p className='homePrimerCompTexto text-align-center '>{servicioActivo.texto}</p>
-              <Link className='buttonContainers'>{servicioActivo.textoButton}</Link>
-            </div>
-          </div>
         </div>
       </div>
-    </div>
-    <div className='wd-100 flex-justify-center containerHeight flex-align-center'>
-              <div className='HomeCuartQuintCompBuscando  flex-align-center flex-justify-space-around'>
 
-                  <div className=' HomeCuartQuintCompBuscandoTitulo'>
-                      <h3 className='titulosComponentes '>Necesitas presupuesto?</h3>
-                      <p className='titulosComponentesTexto '>Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                          Deserunt sunt nihil quod accusamus, vel quis, consequatur amet itaque animi ipsum magni earum impedit nesciunt voluptatibus repellat fuga unde eos. Impedit.</p>
-                  </div>
-                  <div className='wd-10 buttonContainers'><Link to={"/ContactoPage"}>Contactanos<ion-icon name="arrow-forward-outline"></ion-icon></Link></div>
-              </div>
+
+      <div>
+
+        {selectedOption === 'Opción 1' && <div>  <div className='wd-100 flex-justify-center '>
+          <div className='wd-100 mantenimientoContainerInfo '>
+            <img className=' mantenimientoContainerInfoImg h-100' src={mantenimientoSeo} alt='' />
+            <div className='mantenimientoContainerInfoTexto  '>
+              <h5 className='titulosComponentes'>Estrategia Seo</h5>
+              <p className='homePrimerCompTexto text-align-center '>adadaadad Lorem ipsum, dolor sit amet consectetur adipisicing elit Deserunt sunt nihil quod accusamus, vel quis, consequatur amet</p>
+              <Link className='buttonContainers'>Ver mas</Link>
+            </div>
           </div>
-  </div>
+        </div>
+        </div>
+        }
+        {selectedOption === 'Opción 2' &&
+         <div> 
+           <div className='wd-100 flex-justify-center '>
+          <div className='wd-100 mantenimientoContainerInfo '>
+            <img className=' mantenimientoContainerInfoImg h-100' src={seoImg} alt='' />
+            <div className='mantenimientoContainerInfoTexto  '>
+              <h5 className='titulosComponentes'>Estrategias Seo</h5>
+              <p className='homePrimerCompTexto text-align-center '>adadaadad Lorem ipsum, dolor sit amet consectetur adipisicing elit Deserunt sunt nihil quod accusamus, vel quis, consequatur amet</p>
+              <Link className='buttonContainers'>Ver mas</Link>
+            </div>
+          </div>
+        </div>
+        </div>}
+        {selectedOption === 'Opción 3' && <div>  <div className='wd-100 flex-justify-center '>
+          <div className='wd-100 mantenimientoContainerInfo '>
+            <img className=' mantenimientoContainerInfoImg h-100' src={desarrolloWeb} alt='' />
+            <div className='mantenimientoContainerInfoTexto  '>
+              <h5 className='titulosComponentes'>Implementacion Seo</h5>
+              <p className='homePrimerCompTexto text-align-center '>adadaadad Lorem ipsum, dolor sit amet consectetur adipisicing elit Deserunt sunt nihil quod accusamus, vel quis, consequatur amet</p>
+              <Link className='buttonContainers'>Ver mas</Link>
+            </div>
+          </div>
+        </div>
+        </div>}
+      </div>
+    </div>
   );
 };
-
-export default SeoBody;
+export default MantenimientoSeo
--- src/components/SolucionesDigitales/SolucionesDigitalesBody.jsx
+++ src/components/SolucionesDigitales/SolucionesDigitalesBody.jsx
@@ -1,69 +1,145 @@
+
+
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { serviciosData } from '../../data/ServiciosData';
-import solucionesDigitalesBody from '../../assets/solucionesDigitalesPushnPull.webp';
+import aplicacionesWebServicio from '../../assets/aplicacionweb.png'
 import whatsapp from "../../assets/whatsapp.png"
+import solucionesDigitalesImg from '../../assets/solucionesDigitalesPushnPull.webp'
+import ecommerceServicio from '../../assets/ecommerceGrafica.png'
+import desarrolloWebServicio from '../../assets/desarrolloweb.png'
+import menuQrServicio from '../../assets/qr.png'
+import migracionalaNubeServicio from '../../assets/migracionalanube.png'
+import disenoWebServicio from '../../assets/diseñoweb.png'
+const MantenimientoSeo = () => {
+  const [selectedOption, setSelectedOption] = useState('Opción 1'); // Inicializando con 'Opción 1'
 
-
-const SolucionesDigitalesBody = () => {
-  const servicios = serviciosData;
-  const [servicioActivo, setServicioActivo] = useState(servicios[0]);
-
-  const handleChangeServicio = (servicio) => {
-    setServicioActivo(servicio);
+  const handleOptionClick = (option) => {
+    setSelectedOption(option);
   };
-
   return (
     <div className='wd-100'>
       <a className='whatsapp' href="https://wa.me/"><img src={whatsapp} alt="" /></a>
-      <div className='HomePrimerComp'>
+      <div className='HomePrimerComp  '>
         <div className='homePrimerCompContainer'>
           <div>
             <h2 className='homePrimerCompTitulo'>Soluciones Digitales</h2>
             <p className='homePrimerCompTexto font-family-Robot'>Push n Pull es una agencia digital basada en datos especializada en brindar soluciones digitales.</p>
           </div>
+
+
+
+
         </div>
-        <div className='homePrimerCompContainerImg h-100 imagenlatidoContainer flex-align-center flex-justify-center'>
-          <img className='wd-100 h-90 homePrimerCompImg imagen-latido' src={solucionesDigitalesBody} alt="" />
+        <div className='homePrimerCompContainerImg h-100 imagenlatidoContainer flex-align-center flex-justify-center  '>
+          <img className='wd-100 h-90  homePrimerCompImg imagen-latido' src={solucionesDigitalesImg} alt="" />
+        </div>
+      </div>
+      <div className='wd-100 flex-justify-center'>
+        <div className='wd-90 flex-justify-center flex-column  mantenimientoContainerHeight'>           <div className='wd-100 flex-justify-center flex-justify-center'>
+          <div className='wd-100 flex-align-center flex-justify-center flex-column'>
+
+            {/* <div className='flex-column wd-100 flex-justify-space-around mantenimientoContainerRow'>                  */}
+            <ul className='flex-column wd-100 flex-justify-space-around mantenimientoContainerRow'>
+              <li className='buttonVermas2 flex-align-center' onClick={() => handleOptionClick('Opción 1')}>Ecommerce</li>
+              <li className='buttonVermas2 flex-align-center' onClick={() => handleOptionClick('Opción 2')}>Desarrollo Web</li>
+              <li className='buttonVermas2 flex-align-center' onClick={() => handleOptionClick('Opción 3')}>Diseño Web</li>
+              <li className='buttonVermas2 flex-align-center' onClick={() => handleOptionClick('Opción 4')}>Aplicaciones Web</li>
+              <li className='buttonVermas2 flex-align-center' onClick={() => handleOptionClick('Opción 5')}>Migracion a la Nube</li>
+              <li className='buttonVermas2 flex-align-center' onClick={() => handleOptionClick('Opción 6')}>Menú Qr</li>
+            </ul>
+
+            {/* </div> */}
+          </div>
+        </div>
         </div>
       </div>
 
-      <div className='wd-100 flex-justify-center'>
-        <div className='wd-90 flex-justify-center flex-column mantenimientoContainerHeight'>
-          <div className='wd-100 flex-justify-center flex-justify-center'>
-            <div className='wd-100 flex-align-center flex-justify-center flex-column'>
-              <div className='flex-column wd-100 flex-justify-space-around mantenimientoContainerRow'>
-                {servicios.map((servicio, index) => (
-                  <button className='buttonVermas2 flex-align-center' key={index} onClick={() => handleChangeServicio(servicio)}>
-                    {servicio.categoria}
-                  </button>
-                ))}
-              </div>
-            </div>
-          </div>
-          <div className='wd-100 flex-justify-center '>
-            <div className='wd-100 mantenimientoContainerInfo '>
-              <img className=' mantenimientoContainerInfoImg h-100' src={servicioActivo.img} alt='' />
-              <div className='mantenimientoContainerInfoTexto'>
-                <h5 className='titulosComponentes'>{servicioActivo.titulo}</h5>
-                <p className='homePrimerCompTexto text-align-center'>{servicioActivo.texto}</p>
-                <Link className='buttonContainers'>{servicioActivo.textoButton}</Link>
-              </div>
+
+      <div>
+
+        {selectedOption === 'Opción 1' && <div>  <div className='wd-100 flex-justify-center '>
+          <div className='wd-100 mantenimientoContainerInfo '>
+            <img className=' mantenimientoContainerInfoImg h-100' src={ecommerceServicio} alt='' />
+            <div className='mantenimientoContainerInfoTexto  '>
+              <h5 className='titulosComponentes'>Ecommerce</h5>
+              <p className='homePrimerCompTexto text-align-center '>adadaadad Lorem ipsum, dolor sit amet consectetur adipisicing elit Deserunt sunt nihil quod accusamus, vel quis, consequatur amet</p>
+              <Link className='buttonContainers'>Ver mas</Link>
             </div>
           </div>
         </div>
-      </div>
-      <div className='wd-100 flex-justify-center containerHeight flex-align-center'>
-        <div className='HomeCuartQuintCompBuscando flex-align-center flex-justify-space-around'>
-          <div className='HomeCuartQuintCompBuscandoTitulo'>
-            <h3 className='titulosComponentes'>Necesitas presupuesto?</h3>
-            <p className='titulosComponentesTexto'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deserunt sunt nihil quod accusamus, vel quis, consequatur amet itaque animi ipsum magni earum impedit nesciunt voluptatibus repellat fuga unde eos. Impedit.</p>
-          </div>
-          <div className='wd-10 buttonContainers'><Link to={"/ContactoPage"}>Contactanos<ion-icon name="arrow-forward-outline"></ion-icon></Link></div>
         </div>
+        }
+        {selectedOption === 'Opción 2' &&
+          <div>
+            <div className='wd-100 flex-justify-center '>
+              <div className='wd-100 mantenimientoContainerInfo '>
+                <img className=' mantenimientoContainerInfoImg h-100' src={desarrolloWebServicio} alt='' />
+                <div className='mantenimientoContainerInfoTexto  '>
+                  <h5 className='titulosComponentes'>Desarrollo Web</h5>
+                  <p className='homePrimerCompTexto text-align-center '>adadaadad Lorem ipsum, dolor sit amet consectetur adipisicing elit Deserunt sunt nihil quod accusamus, vel quis, consequatur amet</p>
+                  <Link className='buttonContainers'>Ver mas</Link>
+                </div>
+              </div>
+            </div>
+          </div>}
+        {/* {selectedOption === 'Opción 3' && <div>  <div className='wd-100 flex-justify-center '>
+          <div className='wd-100 mantenimientoContainerInfo '>
+            <img className=' mantenimientoContainerInfoImg h-100' src={disenoWebServicio} alt='' />
+            <div className='mantenimientoContainerInfoTexto  '>
+              <h5 className='titulosComponentes'>Diseño Web</h5>
+              <p className='homePrimerCompTexto text-align-center '>adadaadad Lorem ipsum, dolor sit amet consectetur adipisicing elit Deserunt sunt nihil quod accusamus, vel quis, consequatur amet</p>
+              <Link className='buttonContainers'>Ver mas</Link>
+            </div>
+          </div>
+        </div>
+        </div>} */}
+        {selectedOption === 'Opción 3' && <div>  <div className='wd-100 flex-justify-center '>
+          <div className='wd-100 mantenimientoContainerInfo '>
+            <img className=' mantenimientoContainerInfoImg h-100' src={disenoWebServicio} alt='' />
+            <div className='mantenimientoContainerInfoTexto  '>
+              <h5 className='titulosComponentes'>Diseño Web</h5>
+              <p className='homePrimerCompTexto text-align-center '>adadaadad Lorem ipsum, dolor sit amet consectetur adipisicing elit Deserunt sunt nihil quod accusamus, vel quis, consequatur amet</p>
+              <Link className='buttonContainers'>Ver mas</Link>
+            </div>
+          </div>
+        </div>
+        </div>}
+        {selectedOption === 'Opción 4' && <div>  <div className='wd-100 flex-justify-center '>
+          <div className='wd-100 mantenimientoContainerInfo '>
+            <img className=' mantenimientoContainerInfoImg h-100' src={aplicacionesWebServicio} alt='' />
+            <div className='mantenimientoContainerInfoTexto  '>
+              <h5 className='titulosComponentes'>Aplicaciones Web</h5>
+              <p className='homePrimerCompTexto text-align-center '>adadaadad Lorem ipsum, dolor sit amet consectetur adipisicing elit Deserunt sunt nihil quod accusamus, vel quis, consequatur amet</p>
+              <Link className='buttonContainers'>Ver mas</Link>
+            </div>
+          </div>
+        </div>
+        </div>}
+        {selectedOption === 'Opción 5' && <div>  <div className='wd-100 flex-justify-center '>
+          <div className='wd-100 mantenimientoContainerInfo '>
+            <img className=' mantenimientoContainerInfoImg h-100' src={migracionalaNubeServicio} alt='' />
+            <div className='mantenimientoContainerInfoTexto  '>
+              <h5 className='titulosComponentes'>Migración a la Nube</h5>
+              <p className='homePrimerCompTexto text-align-center '>adadaadad Lorem ipsum, dolor sit amet consectetur adipisicing elit Deserunt sunt nihil quod accusamus, vel quis, consequatur amet</p>
+              <Link className='buttonContainers'>Ver mas</Link>
+            </div>
+          </div>
+        </div>
+        </div>}
+        {selectedOption === 'Opción 6' && <div>  <div className='wd-100 flex-justify-center '>
+          <div className='wd-100 mantenimientoContainerInfo '>
+            <img className=' mantenimientoContainerInfoImg h-100' src={menuQrServicio} alt='' />
+            <div className='mantenimientoContainerInfoTexto  '>
+              <h5 className='titulosComponentes'>Menu Qr</h5>
+              <p className='homePrimerCompTexto text-align-center '>adadaadad Lorem ipsum, dolor sit amet consectetur adipisicing elit Deserunt sunt nihil quod accusamus, vel quis, consequatur amet</p>
+              <Link className='buttonContainers'>Ver mas</Link>
+            </div>
+          </div>
+        </div>
+        </div>}
+
       </div>
     </div>
   );
 };
-
-export default SolucionesDigitalesBody;
+export default MantenimientoSeo
