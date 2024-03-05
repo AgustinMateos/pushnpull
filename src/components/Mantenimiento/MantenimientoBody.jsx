0b7b14118fa0815a1d2261106187fdb1128cb03a
agustin mateos <agustinrotondomateos@gmail.com>
2024-03-05 00:10:46 -0300

    mantenimiento

--- src/components/Mantenimiento/MantenimientoBody.jsx
+++ src/components/Mantenimiento/MantenimientoBody.jsx
@@ -2,13 +2,140 @@
 
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { mantenimientoData } from '../../data/MantenimientoData';
-import solucionesDigitalesImg from '../../assets/solucionesDigitalesPushnPull.webp';
+
+import seoImg from '../../assets/ecommerceGrafica.png';
 import seo from '../../assets/ecommerceGrafica.png'
 import whatsapp from "../../assets/whatsapp.png"
+import solucionesDigitalesImg from '../../assets/solucionesDigitalesPushnPull.webp'
 
 
 
+const MantenimientoBody = () => {
+  const [selectedOption, setSelectedOption] = useState('Opción 1'); // Inicializando con 'Opción 1'
+
+  const handleOptionClick = (option) => {
+    setSelectedOption(option);
+  };
+  return (
+    <div className='wd-100'>
+      <a className='whatsapp' href="https://wa.me/"><img src={whatsapp} alt="" /></a>
+      <div className='HomePrimerComp  '>
+        <div className='homePrimerCompContainer'>
+          <div>
+            <h2 className='homePrimerCompTitulo'>Mantenimiento</h2>
+            <p className='homePrimerCompTexto font-family-Robot'>Push n Pull es una agencia digital basada en datos especializada en brindar soluciones digitales.</p>
+          </div>
+
+
+
+
+        </div>
+        <div className='homePrimerCompContainerImg h-100 imagenlatidoContainer flex-align-center flex-justify-center  '>
+          <img className='wd-100 h-90  homePrimerCompImg imagen-latido' src={solucionesDigitalesImg} alt="" />
+        </div>
+      </div>
+      <div className='wd-100 flex-justify-center'>
+        <div className='wd-90 flex-justify-center flex-column  mantenimientoContainerHeight'>           <div className='wd-100 flex-justify-center flex-justify-center'>
+          <div className='wd-100 flex-align-center flex-justify-center flex-column'>
+
+            {/* <div className='flex-column wd-100 flex-justify-space-around mantenimientoContainerRow'>                  */}
+            <ul className='flex-column wd-100 flex-justify-space-around mantenimientoContainerRow'>
+              <li className='buttonVermas2 flex-align-center' onClick={() => handleOptionClick('Opción 1')}>Opción 1</li>
+              <li className='buttonVermas2 flex-align-center' onClick={() => handleOptionClick('Opción 2')}>Opción 2</li>
+              <li className='buttonVermas2 flex-align-center' onClick={() => handleOptionClick('Opción 3')}>Opción 3</li>
+            </ul>
+
+            {/* </div> */}
+          </div>
+        </div>
+        </div>
+      </div>
+
+
+      <div>
+
+        {selectedOption === 'Opción 1' && <div>  <div className='wd-100 flex-justify-center '>
+          <div className='wd-100 mantenimientoContainerInfo '>
+            <img className=' mantenimientoContainerInfoImg h-100' src={seoImg} alt='' />
+            <div className='mantenimientoContainerInfoTexto  '>
+              <h5 className='titulosComponentes'>Estrategia Seo</h5>
+              <p className='homePrimerCompTexto text-align-center '>adadaadad Lorem ipsum, dolor sit amet consectetur adipisicing elit Deserunt sunt nihil quod accusamus, vel quis, consequatur amet</p>
+              <Link className='buttonContainers'>Ver mas</Link>
+            </div>
+          </div>
+        </div>
+        </div>
+        }
+        {selectedOption === 'Opción 2' &&
+         <div> 
+           <div className='wd-100 flex-justify-center '>
+          <div className='wd-100 mantenimientoContainerInfo '>
+            <img className=' mantenimientoContainerInfoImg h-100' src={seoImg} alt='' />
+            <div className='mantenimientoContainerInfoTexto  '>
+              <h5 className='titulosComponentes'>Estrategia Seo</h5>
+              <p className='homePrimerCompTexto text-align-center '>adadaadad Lorem ipsum, dolor sit amet consectetur adipisicing elit Deserunt sunt nihil quod accusamus, vel quis, consequatur amet</p>
+              <Link className='buttonContainers'>Ver mas</Link>
+            </div>
+          </div>
+        </div>
+        </div>}
+        {selectedOption === 'Opción 3' && <div>  <div className='wd-100 flex-justify-center '>
+          <div className='wd-100 mantenimientoContainerInfo '>
+            <img className=' mantenimientoContainerInfoImg h-100' src={seoImg} alt='' />
+            <div className='mantenimientoContainerInfoTexto  '>
+              <h5 className='titulosComponentes'>Estrategia Seo</h5>
+              <p className='homePrimerCompTexto text-align-center '>adadaadad Lorem ipsum, dolor sit amet consectetur adipisicing elit Deserunt sunt nihil quod accusamus, vel quis, consequatur amet</p>
+              <Link className='buttonContainers'>Ver mas</Link>
+            </div>
+          </div>
+        </div>
+        </div>}
+      </div>
+    </div>
+  );
+};
+
+export default MantenimientoBody;
+
+
+
+// function MantenimientoBody() {
+//   const [selectedOption, setSelectedOption] = useState('Opción 1'); // Inicializando con 'Opción 1'
+
+//   const handleOptionClick = (option) => {
+//     setSelectedOption(option);
+//   };
+
+//   return (
+//     <div>
+//       <h1>Menú</h1>
+//       <ul>
+//         <li onClick={() => handleOptionClick('Opción 1')}>Opción 1</li>
+//         <li onClick={() => handleOptionClick('Opción 2')}>Opción 2</li>
+//         <li onClick={() => handleOptionClick('Opción 3')}>Opción 3</li>
+//       </ul>
+//       <div>
+//         <h2>Información de {selectedOption}</h2>
+//         {/* Aquí puedes renderizar la información según la opción seleccionada */}
+//         {selectedOption === 'Opción 1' && <div>  <div className='wd-100 flex-justify-center '>
+//              <div className='wd-100 mantenimientoContainerInfo '>
+//                <img className=' mantenimientoContainerInfoImg h-100' src={seoImg} alt='' />
+//                <div className='mantenimientoContainerInfoTexto  '>
+//                  <h5 className='titulosComponentes'>Estrategia Seo</h5>
+//                 <p className='homePrimerCompTexto text-align-center '>adadaadad Lorem ipsum, dolor sit amet consectetur adipisicing elit Deserunt sunt nihil quod accusamus, vel quis, consequatur amet</p>
+//                <Link className='buttonContainers'>Ver mas</Link>
+//               </div>
+//             </div>
+//           </div> 
+// </div>}
+//         {selectedOption === 'Opción 2' && <img src={seo}/>}
+//         {selectedOption === 'Opción 3' && <p>Información de la opción 3.</p>}
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default MantenimientoBody;
 // const MantenimientoBody = () => {
 //   const servicios = mantenimientoData;
 //   const [servicioActivo, setServicioActivo] = useState(servicios[0]);
@@ -100,33 +227,3 @@ import whatsapp from "../../assets/whatsapp.png"
 // };
 
 // export default MantenimientoBody;
-
-
-
-function MantenimientoBody() {
-  const [selectedOption, setSelectedOption] = useState('Opción 1'); // Inicializando con 'Opción 1'
-
-  const handleOptionClick = (option) => {
-    setSelectedOption(option);
-  };
-
-  return (
-    <div>
-      <h1>Menú</h1>
-      <ul>
-        <li onClick={() => handleOptionClick('Opción 1')}>Opción 1</li>
-        <li onClick={() => handleOptionClick('Opción 2')}>Opción 2</li>
-        <li onClick={() => handleOptionClick('Opción 3')}>Opción 3</li>
-      </ul>
-      <div>
-        <h2>Información de {selectedOption}</h2>
-        {/* Aquí puedes renderizar la información según la opción seleccionada */}
-        {selectedOption === 'Opción 1' &&  <img src={solucionesDigitalesImg } />}
-        {selectedOption === 'Opción 2' && <img src={seo}/>}
-        {selectedOption === 'Opción 3' && <p>Información de la opción 3.</p>}
-      </div>
-    </div>
-  );
-}
-
-export default MantenimientoBody;
