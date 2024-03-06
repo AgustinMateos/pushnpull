5dbeafe55250fba63fadc792c8374b1704bde0d7
agustin mateos <agustinrotondomateos@gmail.com>
2024-03-05 23:42:07 -0300

    pedir presupuespuesto componente

--- src/components/Mantenimiento/MantenimientoBody.jsx
+++ src/components/Mantenimiento/MantenimientoBody.jsx
@@ -93,6 +93,17 @@ const MantenimientoBody = () => {
         </div>
         </div>}
       </div>
+      <div className='wd-100 flex-justify-center containerHeight flex-align-center'>
+         <div className='HomeCuartQuintCompBuscando  flex-align-center flex-justify-space-around'>
+
+           <div className=' HomeCuartQuintCompBuscandoTitulo'>
+             <h3 className='titulosComponentes '>Necesitas presupuesto?</h3>
+             <p className='titulosComponentesTexto '>Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Deserunt sunt nihil quod accusamus, vel quis, consequatur amet itaque animi ipsum magni earum impedit nesciunt voluptatibus repellat fuga unde eos. Impedit.</p>
+          </div>
+         <div className='wd-10 buttonContainers'><Link to={"/ContactoPage"}>Contactanos<ion-icon name="arrow-forward-outline"></ion-icon></Link></div>
+       </div>
+       </div>
     </div>
   );
 };
