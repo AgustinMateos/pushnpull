b276e37d1f5ddb539e02743423305a079c2bcf6c
agustin mateos <agustinrotondomateos@gmail.com>
2024-03-07 18:44:34 -0300

    index mobile

--- src/components/Generals/cards/cardsServicios.jsx
+++ src/components/Generals/cards/cardsServicios.jsx
@@ -18,40 +18,40 @@ const SliderComponent = () => {
   return (
     <div className="slider-container">
       <Slider {...settings}>
-      <Link to={'/SolucionesDigitalesPage'} className='card'>
+      <Link to={'/SolucionesDigitalesPage'} className='card2'>
                     <div className='content contentFlex'>
                         <h3 className='font-family-Robot cardsComponentesTitulos'>Soluciones digitales</h3>
 
                         <div className='price'></div>
                         <ul>
                             <li>
-                                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui voluptatem cum animi minus. Dignissimos vel nihil, laudantium vitae expedita et culpa illo similique totam, aperiam molestias suscipit quos non repellat!</p>
+                                <p className='cardContentContainer'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui voluptatem cum animi minus. Dignissimos vel nihil, laudantium vitae expedita et culpa illo similique totam, aperiam molestias suscipit quos non repellat!</p>
                             </li>
 
                         </ul>
                         <button className='buttonVermas'><span className='buttonVermasContent'>Ver mas <ion-icon name="arrow-forward-outline"></ion-icon></span></button>
                     </div>
                 </Link>
-                <Link to={"/seoPage"} className='card'>
+                <Link to={"/seoPage"} className='card2'>
                     <div className='content contentFlex'>
                         <h3 className='cardsComponentesTitulos'>Seo</h3>
 
                         <div className='price'></div>
                         <ul>
-                            <li><p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui voluptatem cum animi minus. Dignissimos vel nihil, laudantium vitae expedita et culpa illo similique totam, aperiam molestias suscipit quos non repellat!</p></li>
+                            <li><p className='cardContentContainer'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui voluptatem cum animi minus. Dignissimos vel nihil, laudantium vitae expedita et culpa illo similique totam, aperiam molestias suscipit quos non repellat!</p></li>
 
                         </ul>
                         <button className='buttonVermas'><span className='buttonVermasContent'>Ver mas <ion-icon name="arrow-forward-outline"></ion-icon></span></button>
                     </div>
                 </Link>
-         <Link to={"/MantenimientoPage"} className='card'>
+         <Link to={"/MantenimientoPage"} className='card2'>
                     <div className='content contentFlex'>
                         <h3 className='cardsComponentesTitulos'>Mantenimiento</h3>
 
                         <div className='price'></div>
                         <ul>
                             <li>
-                                <p>Lorem ipsum dolor sit, ame t consectetur adipisicing elit.
+                                <p className='cardContentContainer'>Lorem ipsum dolor sit, ame t consectetur adipisicing elit.
                                     Qui voluptatem cum animi minus. Dignissimos
                                     vel nihil, laudantium vitae expedita et culpa illo similique
                                     totam, aperiam molestias suscipit quos non repellat!</p>
