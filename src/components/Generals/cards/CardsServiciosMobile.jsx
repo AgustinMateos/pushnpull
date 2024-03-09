60697f8927602c6041e1b95de694fd6595ca1ece
agustin mateos <agustinrotondomateos@gmail.com>
2024-03-09 01:36:32 -0300

    mobile

--- src/components/Generals/cards/CardsServiciosMobile.jsx
+++ src/components/Generals/cards/CardsServiciosMobile.jsx
@@ -33,7 +33,7 @@ const CardsServiciosMobile = () => {
                     </div>
                 </Link>
                 <Link to={"/seoPage"} className='card3 navbarBackgroundDropImg '>
-                    <div className='wd-100 cardSolucionesDigImgContainer objeto'><img src={seoServicioImg} /></div>
+                    <div className='wd-100 cardSolucionesDigImgContainer '><img src={seoServicioImg} /></div>
                 
                     <div className='content contentFlex3 navbarBackgroundDrop'>
                         <h3 className='cardsComponentesTitulos'>Seo</h3>
@@ -46,26 +46,21 @@ const CardsServiciosMobile = () => {
                         <button className='buttonVermas'><span className='buttonVermasContent'>Ver mas <ion-icon name="arrow-forward-outline"></ion-icon></span></button>
                     </div>
                 </Link>
-         <Link to={"/MantenimientoPage"} className='card3 navbarBackgroundDropImg '>
-           
-               
-                    <div className='content contentFlex3 navbarBackgroundDrop'> <div className='wd-100 cardSolucionesDigImgContainer'> <img src={mantenimientoWebSerivicioImg} /></div>
+                <Link to={"/MantenimientoPage"} className='card3 navbarBackgroundDropImg '>
+                    <div className='wd-100 cardSolucionesDigImgContainer '><img src={mantenimientoWebSerivicioImg} /></div>
+                
+                    <div className='content contentFlex3 navbarBackgroundDrop'>
                         <h3 className='cardsComponentesTitulos'>Mantenimiento</h3>
-
                         <div className='price'></div>
                         <ul>
                             <li>
-                                <p className='cardContentContainer'>Lorem ipsum dolor sit, ame t consectetur adipisicing elit.
-                                    Qui voluptatem cum animi minus. Dignissimos
-                                    vel nihil, laudantium vitae expedita et culpa illo similique
-                                    totam, aperiam molestias suscipit quos non repellat!</p>
+                                <p className='cardContentContainer'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui voluptatem cum animi minus. Dignissimos vel nihil, laudantium vitae expedita et culpa illo similique totam, aperiam molestias suscipit quos non repellat!</p>
                             </li>
-
                         </ul>
                         <button className='buttonVermas'><span className='buttonVermasContent'>Ver mas <ion-icon name="arrow-forward-outline"></ion-icon></span></button>
                     </div>
-                    
                 </Link>
+        
                 
       </Slider>
     </div>
