aa56b087e2a2c80667bfcc71c0246fea80819e70
agustin mateos <agustinrotondomateos@gmail.com>
2024-03-10 21:38:30 -0300

    sections digitales

--- src/components/Mantenimiento/MantenimientoBaseDeDatos.jsx
+++ src/components/Mantenimiento/MantenimientoBaseDeDatos.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 import whatsapp from "../../assets/whatsapp.png"
 import aplicacionesWebServicio from '../../assets/aplicacionweb.png'
 
@@ -58,7 +58,17 @@ const MantenimientoBaseDeDatos = () => {
          
         </div>
       </div>
-      
+      <div className='wd-100 flex-justify-center containerHeight flex-align-center'>
+        <div className='HomeCuartQuintCompBuscando flex-align-center flex-justify-space-around'>
+          <div className='HomeCuartQuintCompBuscandoTitulo'>
+            <h3 className='titulosComponentes font-family-Robot'>Necesitas presupuesto?</h3>
+            <p className='titulosComponentesTexto font-family-Robot'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deserunt sunt nihil quod accusamus, vel quis, consequatur amet itaque animi ipsum magni earum impedit nesciunt voluptatibus repellat fuga unde eos. Impedit.</p>
+          </div>
+          <div className='wd-10 buttonContainers'>
+            <Link to={"/ContactoPage"}>Contactanos<ion-icon name="arrow-forward-outline"></ion-icon></Link>
+          </div>
+        </div>
+      </div>
     </div>
 
   )
--- src/components/SolucionesDigitales/Ecommerce.jsx
+++ src/components/SolucionesDigitales/Ecommerce.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import whatsapp from "../../assets/whatsapp.png"
 import ecommerceServicio from '../../assets/ecommerceGrafica.png'
+import { Link } from 'react-router-dom'
 
 
 const Ecommerce = () => {
@@ -58,7 +59,17 @@ const Ecommerce = () => {
          
         </div>
       </div>
-      
+      <div className='wd-100 flex-justify-center containerHeight flex-align-center'>
+        <div className='HomeCuartQuintCompBuscando flex-align-center flex-justify-space-around'>
+          <div className='HomeCuartQuintCompBuscandoTitulo'>
+            <h3 className='titulosComponentes font-family-Robot'>Necesitas presupuesto?</h3>
+            <p className='titulosComponentesTexto font-family-Robot'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deserunt sunt nihil quod accusamus, vel quis, consequatur amet itaque animi ipsum magni earum impedit nesciunt voluptatibus repellat fuga unde eos. Impedit.</p>
+          </div>
+          <div className='wd-10 buttonContainers'>
+            <Link to={"/ContactoPage"}>Contactanos<ion-icon name="arrow-forward-outline"></ion-icon></Link>
+          </div>
+        </div>
+      </div>
     </div>
 
   )
