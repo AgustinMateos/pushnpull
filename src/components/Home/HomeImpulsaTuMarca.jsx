85ff67b2f187f8d3cf010dfa6f4b94370042896c
agustin mateos <agustinrotondomateos@gmail.com>
2024-03-30 01:30:27 -0300

    style impulsa tu marca

--- src/components/Home/HomeImpulsaTuMarca.jsx
+++ src/components/Home/HomeImpulsaTuMarca.jsx
@@ -15,9 +15,9 @@ const HomeCuartQuintComp = () => {
                     <div class="imagen-container">
                         <h3 className="texto titulosComponentes">Impulsa tu marca</h3>
                         <p className='impulsaTuMarcaTexto'>Especialmente para aquellos emprendedores
-                         y empresas que están buscando llevar su marca al siguiente nivel. Sabemos lo desafiante 
-                         que puede ser comenzar desde cero o darle un nuevo impulso a
-                         tu negocio, y estamos aquí para ayudarte a dar el primer paso.</p>
+                            y empresas que están buscando llevar su marca al siguiente nivel. Sabemos lo desafiante
+                            que puede ser comenzar desde cero o darle un nuevo impulso a
+                            tu negocio, y estamos aquí para ayudarte a dar el primer paso.</p>
                         <Link to={'/ImpulsaTuMarcaPage'} className='buttonContainers'>Ver mas <ion-icon name="arrow-forward-outline"></ion-icon></Link>
                     </div>
 
